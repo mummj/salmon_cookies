a8b7ea34e6b8d30efd0b5362cd2832e1d57598b7
mummj <mummj@eou.edu>
2023-02-15 07:57:09 -0800

    constructor function starter code added

--- js/sales.js
+++ js/sales.js
@@ -1,5 +1,5 @@
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-const tableElement = document.getElementById('sales-table');
+// const tableElement = document.getElementById('sales-table');
 
 // TODO:  create a comment below describing how this Constructor function works
 // - what makes it a constructor function?
